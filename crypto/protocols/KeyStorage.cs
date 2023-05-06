069e83be6e7d31090763bba79078fca8e86d04dc
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2023-05-06 02:06:32 +0200

    port LaVache KeyStorage solution

---
+++ crypto/protocols/KeyStorage.cs
@@ -0,0 +1,120 @@
+﻿using Dzik.crypto.algorithms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dzik.crypto.protocols
+{
+    internal class KeyStorage
+    {
+        private string KeyFilePath;
+        public KeyStorage(string keyFilePath)
+        {
+            KeyFilePath = keyFilePath;
+        }
+
+        internal bool IsKeyStored()
+        {
+            return File.Exists(KeyFilePath);
+        }
+
+        internal bool StoreKey(byte[] key, string password)
+        {
+            try
+            {
+                var encryptedKey = PasswordBasedEncryptor.Encrypt(key, password);
+                File.WriteAllBytes(KeyFilePath, encryptedKey);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        internal bool StoreKey(byte[] key, byte[] KEK)
+        {
+            try
+            {
+                var encryptedKey = AesTool.Encrypt(KEK, key);
+                File.WriteAllBytes(KeyFilePath, encryptedKey);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        internal byte[] ReadKeyBytes(string password)
+        {
+            var keyFileContents = File.ReadAllBytes(KeyFilePath);
+            return PasswordBasedEncryptor.Decrypt(keyFileContents, password);
+        }
+
+        internal PinnedBytes ReadPinnedKeyBytes(SecureString password)
+        {
+            var keyFileContents = File.ReadAllBytes(KeyFilePath);
+            return PasswordBasedEncryptor.DecryptPinned(keyFileContents, password);
+        }
+
+        internal byte[] ReadKeyBytes(byte[] KEK)
+        {
+            var keyFileContents = File.ReadAllBytes(KeyFilePath);
+            return AesTool.Decrypt(KEK, keyFileContents);
+        }
+
+        internal bool RemoveKey()
+        {
+            try
+            {
+                File.Delete(KeyFilePath);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <returns>key used for encrypting the data</returns>
+        internal byte[] BackupTheKey(string backupFilePath)
+        {
+            try
+            {
+                if (!IsKeyStored()) return null;
+
+                var fileBytes = File.ReadAllBytes(KeyFilePath);
+
+                var (encryptedBytes, key) = RandomKeyBasedEncryptor.Encrypt(fileBytes);
+                if (encryptedBytes == null) return null;
+
+                File.WriteAllBytes(backupFilePath, encryptedBytes);
+
+                return key;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        internal bool RestoreBackup(string path, byte[] backupKey)
+        {
+            if (IsKeyStored()) return false;
+
+            var fileBytes = File.ReadAllBytes(path);
+
+            var decryptedFileBytes = RandomKeyBasedEncryptor.Decrypt(backupKey, fileBytes);
+            if (decryptedFileBytes == null) return false;
+
+            File.WriteAllBytes(KeyFilePath, decryptedFileBytes);
+
+            return true;
+        }
+    }
+}
---
+++ crypto/protocols/RandomKeyBasedEncryptor.cs
@@ -0,0 +1,27 @@
+﻿using Dzik.crypto.algorithms;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dzik.crypto.protocols
+{
+    internal static class RandomKeyBasedEncryptor
+    {
+        /// <returns>Encrypted data (or null, if encryption fails) and the key used to encrypt it</returns>
+        internal static (byte[], byte[]) Encrypt(byte[] data)
+        {
+            var key = new byte[Constants.AesKeysLenBytes];
+            RandomNumberGenerator.Create().GetBytes(key);
+
+            var encryptedData = AesTool.Encrypt(key, data);
+
+            return (encryptedData, key);
+        }
+
+        internal static byte[] Decrypt(byte[] key, byte[] encryptedData) => AesTool.Decrypt(key, encryptedData);
+    }
+}
