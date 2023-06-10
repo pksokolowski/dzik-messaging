1662c1e9d46a60594904b88de92e991c70cc2a25
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2023-06-10 14:30:05 +0200

    make reply window a bit prettier

--- ReplyWindow.xaml.cs
+++ ReplyWindow.xaml.cs
@@ -23,11 +23,10 @@ namespace Dzik
     /// </summary>
     public partial class ReplyWindow : Window
     {
-       
+
         internal ReplyWindow(List<MsgPart> msgParts)
         {
             InitializeComponent();
-            SourceInitialized += MainWindow_SourceInitialized;
 
             SourceInitialized += MainWindowRoot_SourceInitialized;
             Closing += MainWindowRoot_Closing;
@@ -49,49 +48,11 @@ namespace Dzik
             });
         }
 
-        // Setup for hiding maximize and minimize buttons on the window.
-        [DllImport("user32.dll")]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        private const int GWL_STYLE = -16;
-
-        private const int WS_MAXIMIZEBOX = 0x10000; //maximize button
-        private const int WS_MINIMIZEBOX = 0x20000; //minimize button
-
-        private IntPtr _windowHandle;
-        private void MainWindow_SourceInitialized(object sender, EventArgs e)
-        {
-            _windowHandle = new WindowInteropHelper(this).Handle;
-
-            //disable minimize button
-            DisableMinimizeButton();
-            DisableMaximizeButton();
-        }
-
-        protected void DisableMinimizeButton()
-        {
-            if (_windowHandle == IntPtr.Zero)
-                throw new InvalidOperationException("The window has not yet been completely initialized");
-
-            SetWindowLong(_windowHandle, GWL_STYLE, GetWindowLong(_windowHandle, GWL_STYLE) & ~WS_MINIMIZEBOX);
-        }
-
-        protected void DisableMaximizeButton()
-        {
-            if (_windowHandle == IntPtr.Zero)
-                throw new InvalidOperationException("The window has not yet been completely initialized");
-
-            SetWindowLong(_windowHandle, GWL_STYLE, GetWindowLong(_windowHandle, GWL_STYLE) & ~WS_MAXIMIZEBOX);
-        }
-
-
         // Remembering window location
         private void MainWindowRoot_SourceInitialized(object sender, EventArgs e)
         {
             this.Top = Settings.Default.ReplyWindowTop;
-            this.Left = Settings.Default.ReplyWindowLeft;         
+            this.Left = Settings.Default.ReplyWindowLeft;
         }
 
         private void MainWindowRoot_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -100,16 +61,25 @@ namespace Dzik
             {
                 // Use the RestoreBounds as the current values will be 0, 0 and the size of the screen
                 Settings.Default.ReplyWindowTop = RestoreBounds.Top;
-                Settings.Default.ReplyWindowLeft = RestoreBounds.Left;         
+                Settings.Default.ReplyWindowLeft = RestoreBounds.Left;
             }
             else
             {
                 Settings.Default.ReplyWindowTop = this.Top;
-                Settings.Default.ReplyWindowLeft = this.Left;         
+                Settings.Default.ReplyWindowLeft = this.Left;
             }
 
             Settings.Default.Save();
         }
 
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
